e1c32697fbc17a134a0a33fdf2c372266fb89ff6
Dan Jarvis <danwakeem@gmail.com>
2023-04-12 15:03:29 -0500

    chore: Updated message

--- services/api-1/index.js
+++ services/api-1/index.js
@@ -2,7 +2,7 @@ module.exports.handler = async (event) => ({
   statusCode: 200,
   body: JSON.stringify(
     {
-      message: 'Go Serverless v3.0! Your function executed successfully!',
+      message: 'HI MOM!',
       input: event,
     },
     null,
